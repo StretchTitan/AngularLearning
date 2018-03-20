fe9c3133b0b04f6d4061b9fef473302d327c9cd2
ChristopherBreit <Christopher.Breit@davita.com>
2018-03-20 11:29:24 -0600

    component data binding intro complete

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,11 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit {
 
-  constructor() { }
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = `Server ${this.serverName} was created!`;
+  }
 }
