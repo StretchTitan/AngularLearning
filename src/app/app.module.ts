cb328233b10c6ee329f401bcdcc1639716d6e479
ChristopherBreit <Christopher.Breit@davita.com>
2019-02-23 08:53:52 -0700

    moved note stuff to it's own branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NoteComponent } from './note/note.component';
-import { NoteTemplateComponent } from './note-template/note-template.component';
-import { NoteRowComponent } from './note/note-row/note-row.component';
-import { CommentComponent } from './note/comment/comment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UserComponent,
-    NoteComponent,
-    NoteTemplateComponent,
-    NoteRowComponent,
-    CommentComponent
+    UserComponent
   ],
   imports: [
     BrowserModule,
